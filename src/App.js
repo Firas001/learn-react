5fb0d94b3f12800b5b53ad96d5b47c64f2f8905c
Firas Al Dweni <al.dweni001@gmail.com>
2023-01-27 20:05:26 +0200

    rtl tailwindcss

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 function App() {
   return (
-    <div className="container m-10 mx-auto rounded-xl border bg-gray-200 p-8 shadow">
-      <p className="mb-5 text-3xl font-bold text-gray-700">Welcome!</p>
-      <p className="text-lg text-gray-500">React and Tailwind CSS in action</p>
+    <div className="container m-10 mx-auto rounded-xl border bg-gray-200 p-8 shadow font-tajawal">
+      <p className="mb-5 text-3xl font-bold text-gray-700">مرحباً!</p>
+      <p className="text-lg text-gray-500">مرحبا بكم في هذا المشروع</p>
     </div>
   );
 }

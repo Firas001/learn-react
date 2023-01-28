1e32955f42aac93f0520496fe970ac5532fd15d3
Firas Al Dweni <al.dweni001@gmail.com>
2023-01-28 21:50:36 +0200

    add components test

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
-function App() {
-  return (
-    <div className="container m-10 mx-auto rounded-xl border bg-gray-200 p-8 shadow font-tajawal">
-      <p className="mb-5 text-3xl font-bold text-gray-700">مرحباً!</p>
-      <p className="text-lg text-gray-500">مرحبا بكم في هذا المشروع</p>
-    </div>
-  );
+import React, { Component } from "react";
+import Learn from "./components/test/Learn";
+import Header from "./components/test/Header";
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <Header />
+        <Learn />
+      </div>
+    );
+  }
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,7 +1,11 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
+import React from "react"; // لازم يكون موجود في كل ملف
+// react include jsx, it likes html
+import ReactDOM from "react-dom/client"; // لازم يكون موجود في كل ملف بينعرض في المتصفح
 import "./index.css";
 import App from "./App";
+
+// ReactDOM.render(what, where);
+// what must have only one tag, or return can only return one tag
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
